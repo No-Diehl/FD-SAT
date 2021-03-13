d3791a144423d4245f70f02a84bb4bc62e5f0dc4
No-Diehl <NorbertDiehl1712@gmail.com>
2021-03-13 12:13:05 +0100

    Cleaned up the planner.cc and moved everything to the sat files

--- src/search/planner.cc
+++ src/search/planner.cc
@@ -2,6 +2,7 @@
 #include "ipasir.h"
 #include "option_parser.h"
 #include "search_engine.h"
+#include "sat/sat.h"
 
 #include "options/registries.h"
 #include "tasks/root_task.h"
@@ -56,80 +57,12 @@ int main(int argc, const char **argv) {
         utils::g_log << "done reading input!" << endl;
         TaskProxy task_proxy(*tasks::g_root_task);
         unit_cost = task_properties::is_unit_cost(task_proxy);
-        // <@=*=@> <@=*=@> <@=*=@> <@=*=@> <@=*=@> <@=*=@> <@=*=@> <@=*=@
-        //FactsProxyIterator fact_it = task_proxy.get_variables().get_facts().end();
-        //cout << AbstractTask(task_proxy).get_variable_name(1) << endl;
-        // cout << task_proxy.get_variables().size() << endl; ---this works!
-		cout << "There are that many initial values: " << task_proxy.get_initial_state().size() << endl;
-		cout << "The initial Values are:" << endl;
-		cout << task_proxy.get_initial_state().get_values() << endl;
-		cout << "Name of variable 18 is: " << task_proxy.get_initial_state()[18].get_name() << endl;
-		cout << "And it's FactPair looks like this: " << task_proxy.get_initial_state()[18].get_pair() << endl;
-		cout << "There are that many goals: " << task_proxy.get_goals().size() << endl;
-		cout << "The goal states are: " << endl;
-		for (std::size_t i=0; i<task_proxy.get_goals().size(); i++) {
-			cout << "Goal " << i+1 << ": " << task_proxy.get_goals()[i].get_name() << " is denoted as " << task_proxy.get_goals()[i].get_pair() << endl;
-		}
-		for (int i=0; i<21; i++) {
-		    cout << "Variable " << task_proxy.get_variables()[i].get_name() << ": Has " << task_proxy.get_variables()[i].get_domain_size() << " items." << endl;
-			for (int j=0; j<task_proxy.get_variables()[i].get_domain_size(); j++) {
-				cout << "Item " << j << " is: " << task_proxy.get_variables()[i].get_fact(j).get_name() << endl;
-			}
-		}
-		cout << "There are " << task_proxy.get_operators().size() << " operators." << endl;
-		cout << "Operator 83 is called: " << task_proxy.get_operators()[82].get_name() << endl;
-		cout << "Operator 83 preconditions are:" << endl;
-		for (std::size_t i=0; i<task_proxy.get_operators()[82].get_preconditions().size(); i++) {
-			cout << task_proxy.get_operators()[82].get_preconditions()[i].get_pair() << " is called " << task_proxy.get_operators()[82].get_preconditions()[i].get_name() << endl;
-		}
-		cout << "Operator 83 effects are:" << endl;
-		for (std::size_t i=0; i<task_proxy.get_operators()[82].get_effects().size(); i++) {
-			cout << task_proxy.get_operators()[82].get_effects()[i].get_fact().get_name() << endl;
-			cout << "Precondition size is: " << task_proxy.get_operators()[82].get_effects()[i].get_conditions().size() << endl;
-		}
-		for (std::size_t o=0; o<task_proxy.get_operators().size(); o++) {
-			cout << "Operator " << o+1 << " is: " << task_proxy.get_operators()[o].get_name() << endl;
-		}
-        for (OperatorProxy const & operators : task_proxy.get_operators()) {
-            cout << operators.get_name() << " ... " << operators.get_id() << endl;
-            cout << "Preconditions are: ";
-            for (FactProxy const & preconditions : operators.get_preconditions()) {
-                cout << preconditions.get_name() << " and ";
-            }
-            cout << endl;
-        }
-        for (VariableProxy const & variables : task_proxy.get_variables()) {
-            cout << variables.get_name() << endl;
-        }
-		//VariableProxy vp = task_proxy.get_variables()[2];
-		//cout << "There are " << task_proxy.get_variables().size() << " variables." << endl;
-		//cout << "Output: " << task_proxy.get_operators()[0].get_effects()[0].get_fact().get_name() << endl;
-		/*for (int n=0; n<9; n++) {
-			FactProxy fp1 = task_proxy.get_goals()[n];
-			cout << "Fact name is: " << fp1.get_name() << endl;
-			for (int o=0; o<2; o++) {
-				FactProxy fp = vp.get_fact(o);
-				cout << "Fact name is: " << fp.get_name() << " and has value: " << fp.get_value() << endl;
-			}
-		} */
-		//FactProxy fp = vp.get_fact(0);
-		//for (int n=0; n<10; n++) {
-		//	cout << vp.get_fact(n).get_pair() << endl;
-		//}
-		//FactPair fpair = fp.get_pair();
-		//cout << fpair << endl;
-		//cout << fp.get_value() << " and has name: " << fp.get_name() << endl;
-		//cout << vp.get_domain_size() << endl; --- this works!
-        //FactsProxyIterator fpi_begin = task_proxy.get_variables().get_facts().begin();
-        //FactsProxyIterator fpi_end = task_proxy.get_variables().get_facts().end();
-        //AbstractTask abs_task(tasks::g_root_task); geht nicht wegen Funktionen
-        
+        sat_init(task_proxy);
+         
     }
 
     // <@=*=@> <@=*=@> <@=*=@> <@=*=@> <@=*=@> <@=*=@> <@=*=@> <@=*=@>
-    
-    //FactProxy     
-    //cout << FactProxy << endl;
+    // End planner here for testing purposes.
     return(0);
     // <@=*=@> <@=*=@> <@=*=@> <@=*=@> <@=*=@> <@=*=@> <@=*=@> <@=*=@>
 
---
+++ src/search/sat/sat.h
@@ -0,0 +1,7 @@
+#include "../ipasir.h"
+#include "../tasks/root_task.h"
+#include "../task_utils/task_properties.h"
+
+#include "../utils/logging.h"
+
+void sat_init(TaskProxy task_proxy);
