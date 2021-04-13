de2182cf99ca2516def584a61034a9d63d1f6880
No-Diehl <NorbertDiehl1712@gmail.com>
2021-04-13 14:15:17 +0200

    Changed place of plan_val function in header file.

--- src/search/sat/sat.h
+++ src/search/sat/sat.h
@@ -15,8 +15,8 @@ void forbidden_binary_states(std::vector<std::vector<std::vector<int>>> & bF);
 void sat_init_binary(TaskProxy task_proxy, sat_capsule & capsule);
 void sat_step(TaskProxy task_proxy, sat_capsule & capsule);
 void sat_step_binary(TaskProxy task_proxy, sat_capsule & capsule);
-void sat_encoding(TaskProxy task_proxy, int steps);
 void output_plan_validate(TaskProxy task_proxy, sat_capsule & capsule, bool b);
+void sat_encoding(TaskProxy task_proxy, int steps);
 void sat_encoding_binary(TaskProxy task_proxy, int steps);
 }
 #endif
